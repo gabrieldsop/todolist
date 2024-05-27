d6a7cdbb8c731366f892c4c1ff9d2eedc5ed2fbc
“gabriel1s1” <gabriel.sisinf@gmail.com>
2024-05-27 20:17:54 -0300

    First Commit

---
+++ src/test/java/br/com/gabriel1s1/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabriel1s1.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
