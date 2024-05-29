090643fb19700f88bf57f769899833e41d7813fb
“gabriel1s1” <gabriel.sisinf@gmail.com>
2024-05-29 18:37:57 -0300

    Retorno Http

--- src/main/java/br/com/gabriel1s1/todolist/user/UserController.java
+++ src/main/java/br/com/gabriel1s1/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.gabriel1s1.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,16 +35,18 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel){
+    public ResponseEntity create(@RequestBody UserModel userModel){
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if(user != null){
-            System.out.println("Usuário já existe");
-            return null;
+
+            // Mensagem de erro
+            // Status Code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
         }
 
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 
 }
